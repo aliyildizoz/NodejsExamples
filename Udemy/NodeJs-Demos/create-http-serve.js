75ddf7e2379522e43bb6e0f7f7dd0b4f0f0a716c
Ali <aliyildizoz909@gmail.com>
2021-07-06 18:50:07 +0300

    index.html served

--- Udemy/NodeJs-Demos/create-http-serve.js
+++ Udemy/NodeJs-Demos/create-http-serve.js
@@ -1,10 +1,16 @@
-const http=require('http');
+const http = require("http");
+const fs = require("fs");
 
-const server=http.createServer((req, res) =>{
-    console.log('Bir istekte bulunuldu');
-    res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'});
-    res.write("<b>Merhaba</b> Dünya");
-    res.end();
-})
+const server = http.createServer((req, res) => {
+  console.log("Bir istekte bulunuldu");
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+
+  fs.readFile("index.html",(err, data) =>{
+      if (err) {
+          throw err;
+      }
+      res.end(data);
+  })
+});
 
 server.listen(3000);
