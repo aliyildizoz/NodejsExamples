0863477b696d37fcad59116c3e4da8d5f6cae99e
Ali <aliyildizoz909@gmail.com>
2021-07-08 17:50:33 +0300

    update

---
+++ Udemy/NodeJs-Demos/express/hello-express.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send('Hello express');
+});
+
+app.listen(3000,()=>{
+    console.log("express has ran")
+});
